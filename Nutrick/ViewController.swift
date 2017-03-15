b499eca22c18efbecab9fb065480d6b684e29ece
Puneet JR <puneetjr@puneets-mbp.amat.com>
2017-03-15 07:24:50 +0530

    Initial Commit

---
+++ Nutrick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nutrick
+//
+//  Created by Puneet JR on 15/03/17.
+//  Copyright © 2017 nutrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
